f29a8a9d3a7875d1eb62e2b13f78dd4882996633
Michael Kramps <mdk989@gmail.com>
2017-09-05 15:12:43 -0500

    added view for squad command

--- server/routes.js
+++ server/routes.js
@@ -3,7 +3,12 @@ var routes = {};
 module.exports = routes;
 
 routes.set = function(app){
+	
 	app.get('/', function(req, res){
 		res.render('static-page');
+	});
+	
+	app.get('/squad-command', function(req, res){
+		res.render('squad-command');
 	});
 }
