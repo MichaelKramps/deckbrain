336fbdfc60e47a5ffbf0c87e36ee22dab40e27ad
Michael Kramps <mdk989@gmail.com>
2017-09-11 21:42:07 -0500

    First attempt at the drafting stage of the game added

--- js/games/robot-royale-data.js
+++ js/games/robot-royale-data.js
@@ -1,28 +1,38 @@
 var numberRobots = 0;
-var bodies = {};
-var armors = {};
-var weapons = {};
+var number = [{name: "1 Robot"}, {name: "2 Robots"}, {name: "3 Robots"}, {name: "4 Robots"}];
+var bodies = [];
+var armors = [];
+var weapons = [];
+var availableChoices = [];
+var choicesMade = [];
 var robot = {};
 var army = [];
 var challenges = {};
 
-var bodies = {
-	tank: {health: 200, speed: 1},
-	thief: {health: 40, speed: 10},
-	fighter: {health: 100, speed: 4}
-};
+var bodies = [
+	{name: "Tank", description: "You can take one heck of a beating", health: 200, speed: 1},
+	{name: "Thief", description: "Good luck landing a punch on this guy", health: 40, speed: 10},
+	{name: "Fighter", description: "Well rounded fighter. Mobile and tough.", health: 100, speed: 4}
+];
 
-var armors = {
-	scavenger: {dampen: 0, scrap: 100},
-	chestplate: {dampen: 6, scrap: 0},
-	magneticplate: {dampen: 2, scrap: 40}
-};
+var armors = [
+	{name: "Scavenger", description: "Use your defeated opponents' scrap metal to repair yourself", dampen: 0, scrap: 80},
+	{name: "Platinum Plate", description: "Lessens the effect of all attacks against you", dampen: 6, scrap: 0},
+	{name: "Magnetic Plate", description: "Attacks are slightly less effective and you can sometimes repair yourself", dampen: 2, scrap: 40}
+];
 
-var weapons = {
-	grenades: {spread: 3, power: 5},
-	superpunch: {spread: 1, power: 12},
-	napalm: {spread: 10, power: 2}
-};
+var weapons = [
+	{name: "Grenades", description: "Respectable damage to a few closeby enemies", spread: 3, power: 5},
+	{name: "Super Punch", description: "Huge amount of damage to one enemy", spread: 1, power: 12},
+	{name: "Napalm", description: "Some damage to lots of enemies", spread: 10, power: 2}
+];
+
+var availableChoices = [
+	{title: "How many robots do you want on your team?", cards: number},
+	{title: "What type of body will your robot team have?", cards: bodies},
+	{title: "What type of armor will your robot team have", cards: armors},
+	{title: "What weapons will they carry?", cards: weapons}
+];
 
 var robot = {
 	weapon: null,
--- js/games/robot-royale.js
+++ js/games/robot-royale.js
@@ -1,12 +1,49 @@
 $(document).ready(function(){
 	
+	var clearPreviousCards = function(){
+		$("#title").empty();
+		$("#cards").children().each(function(){
+			$(this).empty();
+		});
+	};
 	
-	
-	var populateNewPicks = function(picksObject){
-		for (var name in picksObject){
-			
+	var returnCardHTML = function(card){
+		var html = "<h3>" + card.name + "</h3>";
+		if (card.description){
+			html += ("<p>" + card.description + "</p>")
 		}
+		return html;
+	};
+	
+	var populateNewCards = function(choices, round){
+		var cardsObject = choices[round];
+		$("#title").append(cardsObject.title);
+		
+		var cardsArray = cardsObject.cards;
+		for (var i = 1; i < cardsArray.length + 1; i++){
+			var thisCard = cardsArray[i - 1];
+			$("#" + i).append(returnCardHTML(thisCard));
+		}
+	};
+	
+	var listenForCardSelection = function(round){
+		$("#cards").children().unbind();
+		$("#cards").children().on("click", function(){
+			var choiceNumber = parseInt($(this).attr("id"));
+			choicesMade[round] = choiceNumber;
+			draft(availableChoices, round + 1);
+		});
+	};
+	
+	var draft = function(choices, round = 0){
+		if (round === choices.length) {return;}
+		
+		clearPreviousCards();
+		populateNewCards(choices, round);
+		listenForCardSelection(round);
 	}
 	
-	populateNewPicks(armors);
+	
+	
+	draft(availableChoices);
 })
