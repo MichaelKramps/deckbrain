65046c488984c703d5f5f86db07a45ef3f47d4e1
Michael Kramps <mdk989@gmail.com>
2017-10-23 14:29:35 -0500

    basic functionality in place. Now it's time to start making the game fun to play

--- js/games/robot-royale.js
+++ js/games/robot-royale.js
@@ -2,3 +2,12 @@ var data = require("./data.js");
 var draft = require("./draft.js");
 
 draft(data.availableChoices);
+
+// Game Ideas
+//
+// Have stages with a few enemies and a boss (victory gives you rewards to choose from)
+// In order to avoid use of a database use progress codes (like Mega Man X)
+//
+// Two game modes (Campaign and Endless Arena)
+// Campaign to more easily gain experience and items
+// Endless arena to measure the strength of your team and get rare items
