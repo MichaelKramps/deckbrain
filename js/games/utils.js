13e93b32250b7ffbf4e83ab6d2ee6d9248c4a0e7
Michael Kramps <mdk989@gmail.com>
2017-11-08 16:32:12 -0600

    notes on unlock codes

--- js/games/utils.js
+++ js/games/utils.js
@@ -23,3 +23,37 @@ utils.setLevel = function (level) {
 utils.getLevel = function () {
 	return parseInt(utils.getCookie("level"));
 }
+
+// password generation
+// 
+// fourth character determines the location of level unlock code
+// 
+// First 10 values are randomly generated (L00AB00I0W)
+// 0's have no information
+// L = Level unlock code location
+// A = Armor unlock code location
+// B = Body unlock code location
+// I = Item unlock code location
+// W = Weapon unlock code location
+// 
+// unlock codes are two characters 0-Z (base 62 numbers)
+// 
+// These codes will become an integer and be compared to
+// the unlock codes given to each item/level
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
