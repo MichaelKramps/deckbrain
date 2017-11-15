61c8ec10f2b320dcf5ba787b9c38da1bf7fe4be8
Michael Kramps <mdk989@gmail.com>
2017-11-15 15:04:42 -0600

    added stat change animations for more items

--- js/games/itemActions.js
+++ js/games/itemActions.js
@@ -4,6 +4,7 @@ module.exports = itemActions;
 
 var $ = require("jquery");
 var showMyTeam = require("./showMyTeam.js");
+var showEnemyTeam = require("./showEnemyTeam.js");
 
 itemActions.sandwiches = function(gameObject, targetArray, callback){
 	var target = targetArray[0];
@@ -34,18 +35,99 @@ itemActions.sandwiches = function(gameObject, targetArray, callback){
 };
 
 itemActions.springs = function(gameObject, targetArray, callback){
-	targetArray[0].body.speed += 1;
-	callback();
+	var target = targetArray[0];
+	
+	var first = function(){
+		targetArray[0].body.speed += 2;
+		showMyTeam.show(gameObject.battlefield.myTeam);
+	};
+	
+	var second = function(){
+		$("#" + target.id + " .robot-name")
+		.append("<span class='change-special'> +2 speed</span>")
+		.find(".change-special")
+		.fadeOut(1500, function(){
+			callback();
+		});
+	}
+	
+	$.when(
+		first()
+	).then(
+		second()
+	);
 };
 
 itemActions.nets = function(gameObject, targetArray, callback){
-	targetArray[0].weapon.power -= 1;
-	callback();
+	var target = targetArray[0];
+	
+	var first = function(){
+		targetArray[0].weapon.power -= 1;
+		showEnemyTeam(gameObject.battlefield.enemyTeam);
+	};
+	
+	var second = function(){
+		$("#" + target.id + " .enemy-name")
+		.append("<span class='change-special'> -1 power</span>")
+		.find(".change-special")
+		.fadeOut(1500, function(){
+			callback();
+		});
+	}
+	
+	$.when(
+		first()
+	).then(
+		second()
+	);
 };
 
 itemActions.jackets = function(gameObject, targetArray, callback){
-	targetArray[0].armor.dampen -= 0.1;
-	callback();
+	var target = targetArray[0];
+	
+	var first = function(){
+		targetArray[0].armor.dampen -= 0.1;
+		showMyTeam.show(gameObject.battlefield.myTeam);
+	};
+	
+	var second = function(){
+		$("#" + target.id + " .robot-name")
+		.append("<span class='change-special'> +1 defense</span>")
+		.find(".change-special")
+		.fadeOut(1500, function(){
+			callback();
+		});
+	}
+	
+	$.when(
+		first()
+	).then(
+		second()
+	);
+};
+
+itemActions.chemicals = function(gameObject, targetArray, callback){
+	var target = targetArray[0];
+	
+	var first = function(){
+		targetArray[0].armor.dampen += 0.1;
+		showEnemyTeam(gameObject.battlefield.enemyTeam);
+	};
+	
+	var second = function(){
+		$("#" + target.id + " .enemy-name")
+		.append("<span class='change-special'> -1 defense</span>")
+		.find(".change-special")
+		.fadeOut(1500, function(){
+			callback();
+		});
+	}
+	
+	$.when(
+		first()
+	).then(
+		second()
+	);
 };
 
 itemActions.smellingSalts = function(gameObject, targetArray, callback){
@@ -60,9 +142,4 @@ itemActions.smellingSalts = function(gameObject, targetArray, callback){
 	} else {
 		callback();
 	}
-};
-
-itemActions.chemicals = function(gameObject, targetArray, callback){
-	targetArray[0].armor.dampen += 0.1;
-	callback();
 };
