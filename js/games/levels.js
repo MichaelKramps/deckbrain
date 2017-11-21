94eeca718b7845259e73393c058753e4a0565580
Michael Kramps <mdk989@gmail.com>
2017-11-21 08:32:14 -0600

    Adding more levels

--- js/games/levels.js
+++ js/games/levels.js
@@ -92,7 +92,21 @@ var levels = [
 			weapon: data.enemyWeapons.slap,
 			ascii: ascii.enemies.satyr
 		}],
-		[{ // 3.2
+		[{ // 3.3
+			name: "School computer",
+			id: "e0",
+			body: data.enemyBodies.blob,
+			armor: data.enemyArmors.leatherJacket,
+			weapon: data.enemyWeapons.breathFire,
+			ascii: ascii.enemies.computer
+		}]
+	]
+	},
+	{ // level 4
+	name: "Dawn of A.I.",
+	unlock: 4,
+	enemies: [ 
+		[{ // 4.1
 			name: "Vending machine",
 			id: "e0",
 			body: data.enemyBodies.flyer,
@@ -108,21 +122,46 @@ var levels = [
 			weapon: data.enemyWeapons.hit,
 			ascii: ascii.enemies.sodaMachine
 		}],
-		[{ // 3.3
-			name: "School computer",
+		[{ // 4.2
+			name: "Pay phone",
 			id: "e0",
 			body: data.enemyBodies.blob,
-			armor: data.enemyArmors.leatherJacket,
-			weapon: data.enemyWeapons.breathFire,
-			ascii: ascii.enemies.computer
-		}]
-	]
-	},
-	{ // level 4
-	name: "Dawn of A.I.",
-	unlock: 4,
-	enemies: [ 
-		
+			armor: data.enemyArmors.magneticMail,
+			weapon: data.enemyWeapons.hardPunch,
+			ascii: ascii.enemies.sodaMachine
+		}],
+		[{ // 4.3
+			name: "Iphone",
+			id: "e0",
+			body: data.enemyBodies.pilot,
+			armor: data.enemyArmors.ironMail,
+			weapon: data.enemyWeapons.punch,
+			ascii: ascii.enemies.sodaMachine
+		},
+		{
+			name: "Galaxy",
+			id: "e1",
+			body: data.enemyBodies.deer,
+			armor: data.enemyArmors.jacket,
+			weapon: data.enemyWeapons.hit,
+			ascii: ascii.enemies.sodaMachine
+		},
+		{ // 4.3
+			name: "Razor",
+			id: "e0",
+			body: data.enemyBodies.pilot,
+			armor: data.enemyArmors.ironMail,
+			weapon: data.enemyWeapons.hardPunch,
+			ascii: ascii.enemies.sodaMachine
+		},
+		{ // 4.3
+			name: "Pixel",
+			id: "e0",
+			body: data.enemyBodies.guy,
+			armor: data.enemyArmors.jacket,
+			weapon: data.enemyWeapons.slap,
+			ascii: ascii.enemies.sodaMachine
+		}],
 	]
 	},
 	{ // level 5
