9719107b29d6191dcae214199326ade372d8e83f
Michael Kramps <mdk989@gmail.com>
2017-11-28 16:29:05 -0600

    Writing down game mechanics

--- js/prototypes/godrone/main.js
+++ js/prototypes/godrone/main.js
@@ -1,3 +1,20 @@
 // GoDrone
 //
 // Going back to the slots mechanic
+// You have a number of slots that you can attach various objects into
+// These objects do things automatically without intervention
+// 1. Guns that do damage each turn
+// 2. Factories that create ships to send on to the battlefield
+// 3. Repair stations that heal damaged units
+// 4. Upgrade stations that improve existing units
+// 5. Anything else you can think of
+//
+// Additional mechanics ideas
+// 1. Common card pool? - you don't get your own deck, you and your opponent select from the same group of cards
+// 2. Lanes - The battle is fought along 3 lanes (like LoL), like 3 mini battles
+// 3. Winning - The game is won when all your opponent's objects have been destroyed
+// 2. Hidden information - Backup objects are hidden until they enter play
+//    - When an object is destroyed, a new object will automatically take its place
+//    - A player can manually destroy his own object to allow the backup to take over
+// 3. Resource allocation - You have energy/resources to spend and get to decide how they are distributed each turn
+//    - Your slotted objects can only activate if they have enough resource allocation
