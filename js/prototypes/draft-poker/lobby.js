cc60a7c9452c33f3b6ca423223230e8334589ee7
Michael Kramps <mdk989@gmail.com>
2017-12-11 11:11:28 -0600

    added draft poker as a new game to prototype

---
+++ js/prototypes/draft-poker/lobby.js
@@ -0,0 +1,46 @@
+// Draft Poker
+// 
+// Each hand is assigned a point value (pair = 3, full house = 8, etc.)
+// 
+// Draw 7 cards in a draft style format
+// Gain points based on the hand you drafted
+// Play a number of rounds (say 16)
+// Add up points from across all rounds
+// Winner is whoever had the most combined points
+// 
+// Some cards that are not draftable this round are shown to everyone
+// 
+// The 3rd, 5th and 6th card drafted by each player are added to the shown cards
+// 
+// Draft picks come in groups of 4
+// the groups of 4 are chosen randomly from this round's draftable cards
+// 
+// Point values
+// Straight flush: 72,000 * highest card // 72000:1
+// Four of a kind: 4,200 * card // 4200:1
+// Full House: 700 ((triple card * 4/5) + (pair card * 1/5)) // 700:1
+// Flush: 500 * highest card // 500:1
+// Straight: 250 * highest card // 250:1
+// Three of a kind: 25 * card value // 50:1
+// Two pair: 10 * (sum of card values) // 20:1
+// One pair: 2 * card value // 2:1(ish)
+// Nothing: 1 // 1:1
+// 
+// 
+// 
+// 2 player drafts (14 total cards)
+// show 20 cards to everyone
+// 
+// 
+// 
+// 4 player drafts (28 total cards)
+// show 10 cards to everyone
+// 
+// 
+// 
+// 6 player drafts (42 total cards)
+// show 5 cards to everyone
+// 
+// 
+// 
+// 
